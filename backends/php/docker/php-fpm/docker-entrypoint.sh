d1ff9ec94b63df3391c9f2ee7f1bdd8fd6090003
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-10-29 11:12:51 +0300

    chore: env file we add in backends/php/docker/php-fpm/docker-entrypoint.sh

--- backends/php/docker/php-fpm/docker-entrypoint.sh
+++ backends/php/docker/php-fpm/docker-entrypoint.sh
@@ -5,13 +5,46 @@ echo "Starting Symfony application..."
 
 cd /var/www
 
-cat > /var/www/.env << EOF
+if [ ! -f /var/www/.env ] then
+    cat >> /var/www/.env << EOF
+# Real environment variables win over .env files.
+#
+# DO NOT DEFINE PRODUCTION SECRETS IN THIS FILE NOR IN ANY OTHER COMMITTED FILES.
+# https://symfony.com/doc/current/configuration/secrets.html
+#
+# Run "composer dump-env prod" to compile .env files for production use (requires symfony/flex >=1.2).
+# https://symfony.com/doc/current/best_practices.html#use-environment-variables-for-infrastructure-configuration
+
+###> symfony/framework-bundle ###
+APP_ENV=dev
+APP_SECRET=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
+###< symfony/framework-bundle ###
+
+###> symfony/routing ###
+# Configure how to generate URLs in non-HTTP contexts, such as CLI commands.
+# See https://symfony.com/doc/current/routing.html#generating-urls-in-commands
 DEFAULT_URI=
+###< symfony/routing ###
+
+###> doctrine/doctrine-bundle ###
+# Format described at https://www.doctrine-project.org/projects/doctrine-dbal/en/latest/reference/configuration.html#connecting-using-a-url
+# IMPORTANT: You MUST configure your server version, either here or in config/packages/doctrine.yaml
+#
+# DATABASE_URL="sqlite:///%kernel.project_dir%/var/data_%kernel.environment%.db"
+# DATABASE_URL="mysql://app:!ChangeMe!@127.0.0.1:3306/app?serverVersion=8.0.32&charset=utf8mb4"
+# DATABASE_URL="mysql://app:!ChangeMe!@127.0.0.1:3306/app?serverVersion=10.11.2-MariaDB&charset=utf8mb4"
+###< doctrine/doctrine-bundle ###
+
+###> nelmio/cors-bundle ###
+CORS_ALLOW_ORIGIN='^https?://(localhost|127\.0\.0\.1|.*\.cloudpub\.ru)(:[0-9]+)?$'
+###< nelmio/cors-bundle ###
+
 DATABASE_URL=postgresql://${DB_USER:-appuser}:${DB_PASSWORD:-apppass}@database:5432/${DB_NAME:-appdb}?serverVersion=17&charset=utf8
 BITRIX24_PHP_SDK_APPLICATION_CLIENT_ID=${CLIENT_ID:-bitrix_application_client_id}
 BITRIX24_PHP_SDK_APPLICATION_CLIENT_SECRET=${CLIENT_SECRET:-bitrix_application_client_secret}
 BITRIX24_PHP_SDK_APPLICATION_SCOPE=${SCOPE:-bitrix_application_scope}
 EOF
+fi
 
 if [ -f "composer.json" ]; then
     echo "Found composer.json, checking dependencies..."
