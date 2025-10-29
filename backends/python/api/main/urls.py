5180b58b49ec089aaffde2bedec4c389a61d2cab
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-10-29 07:44:47 +0300

    chore: update urlpatterns

--- backends/python/api/main/urls.py
+++ backends/python/api/main/urls.py
@@ -4,8 +4,8 @@ from .views import *
 urlpatterns = [
     path('api', root, name='root'),
     path('api/health', health, name='health'),
-    path('api/get_enum', get_enum, name='enum'),
-    path('api/get_list', get_list, name='list'),
+    path('api/enum', get_enum, name='enum'),
+    path('api/list', get_list, name='list'),
     path('api/install', install, name='install'),
     path('api/getToken', get_token, name='get_token'),
 ]
