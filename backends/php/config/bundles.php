00b90a6d11f708ff89ad0ba11b1435eac748b4fb
mesilov <mesilov.maxim@gmail.com>
2025-11-04 00:59:03 +0600

    Enforced `strict_types` declaration in PHP config files for stricter type checking.
    Signed-off-by: mesilov <mesilov.maxim@gmail.com>

--- backends/php/config/bundles.php
+++ backends/php/config/bundles.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
